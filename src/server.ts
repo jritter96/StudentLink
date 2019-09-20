0065f71a58a09178ba2a82bcf34db3e520c4cad9
Connor Fong <connor_fong@hotmail.com>
2019-09-19 21:47:48 -0700

    17: Add Place for API Documentation

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import * as express from 'express';
+import * as swaggerUi from 'swagger-ui-express';
 import * as classRouter from './routes/class';
 import * as userRouter from './routes/user';
 
 const server = express();
+const swaggerDoc = require('../swagger.json');
 const ENDPOINT = process.env.ENDPOINT || 'http://localhost';
 const PORT = process.env.PORT || 3000;
 
-server.use('/users', userRouter);
-server.use('/classes', classRouter);
+server.use('/user', userRouter);
+server.use('/class', classRouter);
 
-server.get('/', (req, res) => {
-    res.send('This is a placeholder for documentation.');
-});
+// host API documentation
+server.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 server.listen(PORT, () => {
     console.log(`Server is running in ${ENDPOINT}:${PORT}`);
