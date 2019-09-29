51033c6255538e673b61f2adcd7064fe11b8fe71
Aaron Congo <aaroncongo@outlook.com>
2019-09-28 17:12:02 -0700

    Added GET and POST requests for courses

--- src/routes/course.ts
+++ src/routes/course.ts
@@ -1,13 +1,40 @@
 import * as express from 'express';
+const Course = require('../models/course');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('GET course using a courseId');
+/* 
+ * Read a course from the database using its Object ID
+ *
+ * GET /course/:id
+ */
+router.get('/:id', async (req, res) => {
+    try {
+        const course = await Course.findOne({ _id: req.params.id });
+
+        if (!course) {
+            return res.status(404).send();
+        }
+
+        res.send(course);
+    } catch (error) {
+        res.status(500).send();
+    }
 });
 
-router.post('/', (req, res) => {
-    // TODO: implement
+/*
+ * Create a course
+ * POST /course
+ */
+router.post('/', async (req, res) => {
+    const course = new Course(req.body);
+
+    try {
+        await course.save();
+        res.status(201).send(course);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 });
 
 module.exports = router;
