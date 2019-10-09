4c574b0db03969d40ae3563ab0c1253074ddd43d
Connor Fong <dconnorfong@gmail.com>
2019-10-08 22:01:01 -0700

    49: PATCH on courses (#52)

--- server/src/routes/course.ts
+++ server/src/routes/course.ts
@@ -37,4 +37,40 @@ router.post('/', async (req, res) => {
     }
 });
 
+/*
+ * Modifies a course
+ * PATCH /course/:id
+ */
+router.patch('/:id', async (req, res) => {
+    // collect all of the requested key updates and validate them against allowed changes
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['times'];
+
+    const isValidOperation = updates.every(update => {
+        return allowedUpdates.includes(update);
+    });
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates requested' });
+    }
+
+    try {
+        const course = await Course.findOne({ _id: req.params.id });
+
+        if (!course) {
+            res.status(404).send();
+        }
+
+        updates.forEach(update => {
+            course[update] = req.body[update];
+        });
+
+        await course.save();
+
+        res.send(course);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
 module.exports = router;
