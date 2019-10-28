d5c3e7c876c4f1825693da17e6c58a0261fd39de
Connor Fong <dconnorfong@gmail.com>
2019-10-28 14:57:56 -0700

    (no ticket) refactor code on navbar (#83)

--- mobile/src/components/Navbar/Navbar.tsx
+++ mobile/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-import {
-    TouchableHighlight,
-    StyleSheet,
-    View,
-    SafeAreaView,
-    Text,
-} from 'react-native';
+import { TouchableHighlight, View, SafeAreaView, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import {
     navbarStyles,
@@ -22,158 +16,114 @@ interface NavBarProps {
 
 /*
  * active: 0 - Group; 1 - Schedule; 2 - Messages; 3 - Settings
+ *
+ * active should correspond to the keys in options
  */
 interface NavBarState {
     active: Number;
+    options: any[];
 }
 
 export default class Navbar extends Component<NavBarProps, NavBarState> {
     constructor(props: NavBarProps) {
         super(props);
-
-        this.handleSelectionGroup = this.handleSelectionGroup.bind(this);
-        this.handleSelectionSchedule = this.handleSelectionSchedule.bind(this);
-        this.handleSelectionMessages = this.handleSelectionMessages.bind(this);
-        this.handleSelectionSettings = this.handleSelectionSettings.bind(this);
+        this.handleSelection = this.handleSelection.bind(this);
+        this.renderNavbar = this.renderNavbar.bind(this);
         this.renderSelectionColor = this.renderSelectionColor.bind(this);
 
         this.state = {
             active: 0,
+            options: [
+                {
+                    key: 0,
+                    title: 'Group',
+                    icon: 'ios-people',
+                },
+                {
+                    key: 1,
+                    title: 'Schedule',
+                    icon: 'ios-calendar',
+                },
+                {
+                    key: 2,
+                    title: 'Messages',
+                    icon: 'ios-text',
+                },
+                {
+                    key: 3,
+                    title: 'Settings',
+                    icon: 'ios-settings',
+                },
+            ],
         };
     }
 
     render() {
         return (
             <SafeAreaView style={navbarStyles.navbarContainer}>
-                <TouchableHighlight
-                    onPress={this.handleSelectionGroup.bind(this)}
-                    style={navbarStyles.navButton}
-                    underlayColor={navbarHighlightColor}
-                >
-                    <View style={navbarStyles.navButtonContainer}>
-                        <View style={navbarStyles.navButtonIconContainer}>
-                            <Ionicons
-                                name="ios-people"
-                                size={30}
-                                color={this.renderSelectionColor(0)}
-                            />
-                        </View>
-                        <View style={navbarStyles.navButtonTextContainer}>
-                            <Text
-                                style={[
-                                    navbarStyles.navButtonText,
-                                    { color: this.renderSelectionColor(0) },
-                                ]}
-                            >
-                                Group
-                            </Text>
-                        </View>
-                    </View>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    onPress={this.handleSelectionSchedule.bind(this)}
-                    style={navbarStyles.navButton}
-                    underlayColor={navbarHighlightColor}
-                >
-                    <View style={navbarStyles.navButtonContainer}>
-                        <View style={navbarStyles.navButtonIconContainer}>
-                            <Ionicons
-                                name="ios-calendar"
-                                size={30}
-                                color={this.renderSelectionColor(1)}
-                            />
-                        </View>
-                        <View style={navbarStyles.navButtonTextContainer}>
-                            <Text
-                                style={[
-                                    navbarStyles.navButtonText,
-                                    { color: this.renderSelectionColor(1) },
-                                ]}
-                            >
-                                Schedule
-                            </Text>
-                        </View>
-                    </View>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    onPress={this.handleSelectionMessages.bind(this)}
-                    style={navbarStyles.navButton}
-                    underlayColor={navbarHighlightColor}
-                >
-                    <View style={navbarStyles.navButtonContainer}>
-                        <View style={navbarStyles.navButtonIconContainer}>
-                            <Ionicons
-                                name="ios-text"
-                                size={30}
-                                color={this.renderSelectionColor(2)}
-                            />
-                        </View>
-                        <View style={navbarStyles.navButtonTextContainer}>
-                            <Text
-                                style={[
-                                    navbarStyles.navButtonText,
-                                    { color: this.renderSelectionColor(2) },
-                                ]}
-                            >
-                                Messages
-                            </Text>
-                        </View>
-                    </View>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    onPress={this.handleSelectionSettings.bind(this)}
-                    style={navbarStyles.navButton}
-                    underlayColor={navbarHighlightColor}
-                >
-                    <View style={navbarStyles.navButtonContainer}>
-                        <View style={navbarStyles.navButtonIconContainer}>
-                            <Ionicons
-                                name="ios-settings"
-                                size={30}
-                                color={this.renderSelectionColor(3)}
-                            />
-                        </View>
-                        <View style={navbarStyles.navButtonTextContainer}>
-                            <Text
-                                style={[
-                                    navbarStyles.navButtonText,
-                                    { color: this.renderSelectionColor(3) },
-                                ]}
-                            >
-                                Settings
-                            </Text>
-                        </View>
-                    </View>
-                </TouchableHighlight>
+                {this.renderNavbar()}
             </SafeAreaView>
         );
     }
 
-    handleSelectionGroup() {
+    // TODO: refactor top level handler to accept an argument (call a single onPressNavButton)
+    handleSelection(selection: Number) {
         this.setState({
-            active: 0,
+            active: selection,
         });
-        this.props.OnPressNavButtonSearch();
+
+        switch (selection) {
+            case 0:
+                this.props.OnPressNavButtonSearch();
+                break;
+            case 1:
+                this.props.OnPressNavButtonSch();
+                break;
+            case 2:
+                this.props.OnPressNavButtonChat();
+                break;
+            case 3:
+                // TODO: route to settings view
+                break;
+            default:
+                // TODO: describe this logic
+                break;
+        }
     }
 
-    handleSelectionSchedule() {
-        this.setState({
-            active: 1,
-        });
-        this.props.OnPressNavButtonSch();
-    }
-
-    handleSelectionMessages() {
-        this.setState({
-            active: 2,
-        });
-        this.props.OnPressNavButtonChat();
-    }
-
-    handleSelectionSettings() {
-        this.setState({
-            active: 3,
-        });
+    renderNavbar() {
+        return this.state.options.map(option => (
+            <TouchableHighlight
+                key={option.key}
+                onPress={this.handleSelection.bind(this, option.key)}
+                style={navbarStyles.navButton}
+                underlayColor={navbarHighlightColor}
+            >
+                <View style={navbarStyles.navButtonContainer}>
+                    <View style={navbarStyles.navButtonIconContainer}>
+                        <Ionicons
+                            name={option.icon}
+                            size={30}
+                            color={this.renderSelectionColor(option.key)}
+                        />
+                    </View>
+                    <View style={navbarStyles.navButtonTextContainer}>
+                        <Text
+                            style={[
+                                navbarStyles.navButtonText,
+                                {
+                                    color: this.renderSelectionColor(
+                                        option.key
+                                    ),
+                                },
+                            ]}
+                        >
+                            {option.title}
+                        </Text>
+                    </View>
+                </View>
+            </TouchableHighlight>
+        ));
     }
 
     renderSelectionColor(active: Number) {
