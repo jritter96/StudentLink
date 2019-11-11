797ba7d58475b40fb49e7beb158a4c540272a6af
Connor Fong <connor_fong@hotmail.com>
2019-11-10 18:20:21 -0800

    Chat App: implemented REST test API

---
+++ server/src/routes/chat.ts
@@ -0,0 +1,46 @@
+import * as express from 'express';
+const Chat = require('../models/chat');
+const router = express.Router();
+
+/*
+ * Chat endpoints used to accelerate testing database functionality
+ *
+ * Normal chat usage should be done only through socket communication within the app
+ */
+
+/*
+ * Get a chat object using its group ID
+ *
+ * GET /chat/:id
+ */
+router.get('/:id', async (req, res) => {
+    try {
+        const chat = await Chat.findOne({ groupId: req.params.id });
+
+        if (!chat) {
+            return res.status(404).send();
+        }
+
+        res.send(chat);
+    } catch (error) {
+        res.status(500).send();
+    }
+});
+
+/*
+ * Create a new chat object
+ *
+ * POST /chat
+ */
+router.post('/', async (req, res) => {
+    const chat = new Chat(req.body);
+
+    try {
+        await chat.save();
+        res.status(201).send(chat);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+module.exports = router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,7 @@ import * as swaggerUi from 'swagger-ui-express';
 import * as courseRouter from './routes/course';
 import * as userRouter from './routes/user';
 import * as groupRouter from './routes/group';
+import * as chatRouter from './routes/chat';
 import { initializeChat } from './chat/chat';
 require('./db/mongoose');
 
@@ -22,6 +23,7 @@ server.use(express.json());
 server.use('/user', userRouter);
 server.use('/course', courseRouter);
 server.use('/group', groupRouter);
+server.use('/chat', chatRouter);
 
 initializeChat(service);
 
