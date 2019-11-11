2e7f3883db8ca5cc0dc2b2da61546670c16b2a35
Joel Ritter <joel.ritter96@gmail.com>
2019-11-10 17:09:03 -0800

    Chatroom updated to use flatlist

--- mobile/src/components/Chat/Chat.tsx
+++ mobile/src/components/Chat/Chat.tsx
@@ -23,6 +23,8 @@ interface ChatProps {
     HandleChatroomClose: Function;
 }
 
+const endpoint = 'http://ec2-18-222-96-240.us-east-2.compute.amazonaws.com';
+
 export default class Chat extends Component<ChatProps> {
     constructor(props: any) {
         super(props);
