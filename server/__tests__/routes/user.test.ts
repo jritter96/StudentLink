7ceb8e0b7a1dfa0598b5011a40ad1d5f99fb76df
Connor Fong <connor_fong@hotmail.com>
2019-11-13 17:07:22 -0800

    server test: user route complete

--- server/__tests__/routes/user.test.ts
+++ server/__tests__/routes/user.test.ts
@@ -1,16 +1,31 @@
 const request = require('supertest');
 const service = require('../../src/service');
+const User = require('../../src/models/user');
+
+const testUserId = '5d8c51d08a7fc5ae1f4531a2';
+const invalidUserId = '000051d08a7fc5ae1f450000';
+
+const testUser = {
+    _id: testUserId,
+    firstName: 'Test',
+    lastName: 'User',
+};
+
+beforeEach(async () => {
+    await User.deleteMany();
+    await new User(testUser).save();
+});
 
 test('Can retrieve a valid user', async () => {
     await request(service)
-        .get('/user/5d8c51d08a7fc5ae1f4531a2')
+        .get(`/user/${testUserId}`)
         .send()
         .expect(200);
 });
 
 test('Can not retrieve a invalid user', async () => {
     await request(service)
-        .get('/user/000051d08a7fc5ae1f450000')
+        .get(`/user/${invalidUserId}`)
         .send()
         .expect(404);
 });
@@ -20,8 +35,8 @@ test('Can login with valid credentials', async () => {
     await request(service)
         .post('/user/login')
         .send({
-            firstName: 'Connor',
-            lastName: 'Fong',
+            firstName: 'Test',
+            lastName: 'User',
         })
         .expect(200);
 });
@@ -33,6 +48,47 @@ test('Can not login with invalid credentials', async () => {
         .send({
             firstName: 'INVALID',
             lastName: 'CREDENTIALS',
+        })
+        .expect(400);
+});
+
+test('Can create a new user', async () => {
+    const newUserId = '5d8c51d08a7fc5ae1f455555';
+
+    const newUser = {
+        _id: newUserId,
+        firstName: 'New',
+        lastName: 'User',
+    };
+
+    // create a new user
+    await request(service)
+        .post('/user')
+        .send(newUser)
+        .expect(201);
+
+    // validate that they are in the database
+    await request(service)
+        .get(`/user/${newUserId}`)
+        .send()
+        .expect(200);
+});
+
+test('Can update an existing user', async () => {
+    const response = await request(service)
+        .patch(`/user/${testUserId}`)
+        .send({
+            firstName: 'Tester',
+        });
+
+    expect(response.body.firstName).toEqual('Tester');
+});
+
+test('Can not update an existing user with invalid fields', async () => {
+    await request(service)
+        .patch(`/user/${testUserId}`)
+        .send({
+            _id: invalidUserId,
         })
         .expect(400);
 });
