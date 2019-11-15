4b171d0c196e13e2577cfe3154dbb3d68d96cf1b
Connor Fong <connor_fong@hotmail.com>
2019-11-14 16:32:50 -0800

    mobile test: init code

---
+++ mobile/e2e/firstTest.spec.js
@@ -0,0 +1,11 @@
+const { reloadApp } = require('detox-expo-helpers');
+
+describe('Test', () => {
+    beforeEach(async () => {
+        await reloadApp();
+    });
+
+    it('Show login buttons', async () => {
+        // TODO: test
+    });
+});
