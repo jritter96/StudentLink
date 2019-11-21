e371ddb99ad75a747bc196b673db790260a0547b
Connor Fong <connor_fong@hotmail.com>
2019-11-20 21:46:19 -0800

    settings: canvas modal open/close controller

--- mobile/src/components/Modal/CanvasModal.tsx
+++ mobile/src/components/Modal/CanvasModal.tsx
@@ -3,7 +3,6 @@ import {
     View,
     SafeAreaView,
     Text,
-    ActivityIndicator,
     ScrollView,
     TouchableOpacity,
     Linking,
@@ -12,90 +11,76 @@ import {
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import config from '../../../config/config';
+import { modalStyles } from '../../styles/modal';
 
-export default class CanvasModal extends Component<{}, {}> {
+interface ICanvasModalProps {
+    visible: boolean;
+    toggleVisible: (toggle: boolean) => void;
+}
+
+export default class CanvasModal extends Component<ICanvasModalProps, {}> {
+    constructor(props) {
+        super(props);
+    }
+
     public render() {
         return (
             <Modal
-                animationType="slide"
+                animationType="fade"
                 transparent={true}
-                visible={true}
-                onRequestClose={() => {
-                    Alert.alert('Modal has been closed.');
-                }}
+                visible={this.props.visible}
             >
-                <SafeAreaView
-                    style={{
-                        height: '100%',
-                        backgroundColor: 'rgba(0,0,0,0.5)',
-                    }}
-                >
-                    <ScrollView
-                        style={{
-                            backgroundColor: '#FFFFFF',
-                            borderRadius: 10,
-                            marginTop: 50,
-                            marginBottom: 100,
-                            marginHorizontal: 20,
-                            paddingHorizontal: 10,
-                        }}
-                    >
+                <SafeAreaView style={modalStyles.container}>
+                    <ScrollView style={modalStyles.content}>
                         <View>
-                            <TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={this.props.toggleVisible.bind(
+                                    this,
+                                    false
+                                )}
+                            >
                                 <Ionicons
                                     name="ios-close"
                                     size={50}
                                     color="#444444"
                                 />
                             </TouchableOpacity>
-                            <Text>Linking your Canvas API Key is easy!</Text>
-                            <Text>
+                            <Text style={modalStyles.titleHeader}>
+                                Linking your Canvas API Key is easy!
+                            </Text>
+                            <Text style={modalStyles.text}>
                                 1. In Settings, navigate to 'New Access Token'
                                 under Approved Integrations
                             </Text>
-                            <Text>{'\n'}</Text>
                             <Image
-                                style={{
-                                    width: null,
-                                    height: 175,
-                                    resizeMode: 'contain',
-                                }}
+                                style={modalStyles.image}
                                 source={require('../../assets/api-1.png')}
                             />
-                            <Text>{'\n'}</Text>
-                            <Text>
+                            <Text style={modalStyles.text}>
                                 2. Optionally set an expiry and select 'Generate
                                 Token'
                             </Text>
-                            <Text>{'\n'}</Text>
                             <Image
-                                style={{
-                                    width: null,
-                                    height: 175,
-                                    resizeMode: 'contain',
-                                }}
+                                style={modalStyles.image}
                                 source={require('../../assets/api-2.png')}
                             />
-                            <Text>{'\n'}</Text>
-                            <Text>3. Copy your new token to clipboard</Text>
-                            <Text>{'\n'}</Text>
+                            <Text style={modalStyles.text}>
+                                3. Copy your new token to clipboard
+                            </Text>
                             <Image
-                                style={{
-                                    width: null,
-                                    height: 175,
-                                    resizeMode: 'contain',
-                                }}
+                                style={modalStyles.image}
                                 source={require('../../assets/api-3.png')}
                             />
-                            <Text>{'\n'}</Text>
-                            <Text
-                                style={{ color: 'blue' }}
-                                onPress={() =>
-                                    Linking.openURL(config.canvasAPI)
-                                }
-                            >
-                                Go to UBC Canvas
-                            </Text>
+                            <View style={modalStyles.buttonContainer}>
+                                <Text
+                                    style={modalStyles.button}
+                                    onPress={() =>
+                                        Linking.openURL(config.canvasAPI)
+                                    }
+                                >
+                                    Go to UBC Canvas
+                                </Text>
+                            </View>
                             <Text>{'\n'}</Text>
                         </View>
                     </ScrollView>
---
+++ mobile/src/styles/modal.ts
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+
+export const modalStyles = StyleSheet.create({
+    container: {
+        height: '100%',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    content: {
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        marginTop: 50,
+        marginBottom: 100,
+        marginHorizontal: 20,
+        paddingHorizontal: 10,
+    },
+    titleHeader: {
+        color: '#444444',
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    text: {
+        color: '#444444',
+        fontSize: 15,
+    },
+    image: {
+        width: null,
+        height: 175,
+        resizeMode: 'contain',
+        marginVertical: 20,
+    },
+    buttonContainer: {
+        flex: 1,
+        borderRadius: 20,
+        backgroundColor: '#947BD3',
+        marginHorizontal: 20,
+        paddingVertical: 10,
+    },
+    button: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});
