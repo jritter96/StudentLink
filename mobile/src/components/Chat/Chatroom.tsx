d9e18ff0a037de5436570a63d42f8b9efb9bb9ce
Connor Fong <connor_fong@hotmail.com>
2019-11-24 12:01:39 -0800

    Chatroom.tsx conforms to linter

--- mobile/src/components/Chat/Chatroom.tsx
+++ mobile/src/components/Chat/Chatroom.tsx
@@ -34,14 +34,6 @@ export default class Chatroom extends Component<ChatroomProps, ChatroomState> {
         this.reloadChatroom = this.reloadChatroom.bind(this);
     }
 
-    private sendMessage() {
-        if (this.state.newMessage.replace(/\s/g, '').length) {
-            this.props.socket.emit("sendMessage", this.props.userID, this.props.groupID, this.state.newMessage, this.props.handleNewMessage);
-            this.setState({ newMessage: "" });
-        }
-        return;
-    }
-
     public reloadChatroom() {
         this.setState({ messages: [...this.props.messages] });
         return;
@@ -55,7 +47,9 @@ export default class Chatroom extends Component<ChatroomProps, ChatroomState> {
             >
                 <View style={chatroomStyles.topContainer}>
                     <View style={chatroomStyles.titleContainer}>
-                        <Text style={chatroomStyles.title}>{this.props.groupName}</Text>
+                        <Text style={chatroomStyles.title}>
+                            {this.props.groupName}
+                        </Text>
                     </View>
                 </View>
                 <View style={chatroomStyles.backButtonContainer}>
@@ -66,53 +60,116 @@ export default class Chatroom extends Component<ChatroomProps, ChatroomState> {
                     </TouchableOpacity>
                 </View>
                 <View style={chatroomStyles.whitespace} />
-                    <View style={chatroomStyles.listContainer}>
-                        <FlatList
-                            inverted
-                            data={this.state.messages.reverse()}
-                            renderItem={({item}) => {
-                                if (item.senderId === this.props.userID) {
-                                    return (
-                                        <View style={chatroomStyles.outgoingMessageContainer}>
-                                            <View style={chatroomStyles.outgoingMessageBox}>
-                                                <Text style={chatroomStyles.messageText}>
-                                                    {item.message}
-                                                </Text>
-                                                <Text style={chatroomStyles.messageTimeStamp}>
-                                                    {new Date(item.createdAt).toLocaleDateString('en-US') + " " + new Date(item.createdAt).toLocaleTimeString('en-US').substring(0, 4) + new Date(item.createdAt).toLocaleTimeString('en-US').substring(7, 11)}
-                                                </Text>
-                                            </View>
+                <View style={chatroomStyles.listContainer}>
+                    <FlatList
+                        inverted
+                        data={this.state.messages.reverse()}
+                        renderItem={({ item }) => {
+                            if (item.senderId === this.props.userID) {
+                                return (
+                                    <View
+                                        style={
+                                            chatroomStyles.outgoingMessageContainer
+                                        }
+                                    >
+                                        <View
+                                            style={
+                                                chatroomStyles.outgoingMessageBox
+                                            }
+                                        >
+                                            <Text
+                                                style={
+                                                    chatroomStyles.messageText
+                                                }
+                                            >
+                                                {item.message}
+                                            </Text>
+                                            <Text
+                                                style={
+                                                    chatroomStyles.messageTimeStamp
+                                                }
+                                            >
+                                                {new Date(
+                                                    item.createdAt
+                                                ).toLocaleDateString('en-US') +
+                                                    ' ' +
+                                                    new Date(item.createdAt)
+                                                        .toLocaleTimeString(
+                                                            'en-US'
+                                                        )
+                                                        .substring(0, 4) +
+                                                    new Date(item.createdAt)
+                                                        .toLocaleTimeString(
+                                                            'en-US'
+                                                        )
+                                                        .substring(7, 11)}
+                                            </Text>
                                         </View>
-                                    )
-                                }
-                                else {
-                                    return (
-                                        <View style={chatroomStyles.incomingMessageContainer}>
-                                            <View style={chatroomStyles.incomingMessageBox}>
-                                                <Text style={chatroomStyles.messageSender}>
-                                                    {item.senderName}
-                                                </Text>
-                                                <Text style={chatroomStyles.messageText}>
-                                                    {item.message}
-                                                </Text>
-                                                <Text style={chatroomStyles.messageTimeStamp}>
-                                                    {new Date(item.createdAt).toLocaleDateString('en-US') + " " + new Date(item.createdAt).toLocaleTimeString('en-US').substring(0, 4) + new Date(item.createdAt).toLocaleTimeString('en-US').substring(7, 11)}
-                                                </Text>
-                                            </View>
+                                    </View>
+                                );
+                            } else {
+                                return (
+                                    <View
+                                        style={
+                                            chatroomStyles.incomingMessageContainer
+                                        }
+                                    >
+                                        <View
+                                            style={
+                                                chatroomStyles.incomingMessageBox
+                                            }
+                                        >
+                                            <Text
+                                                style={
+                                                    chatroomStyles.messageSender
+                                                }
+                                            >
+                                                {item.senderName}
+                                            </Text>
+                                            <Text
+                                                style={
+                                                    chatroomStyles.messageText
+                                                }
+                                            >
+                                                {item.message}
+                                            </Text>
+                                            <Text
+                                                style={
+                                                    chatroomStyles.messageTimeStamp
+                                                }
+                                            >
+                                                {new Date(
+                                                    item.createdAt
+                                                ).toLocaleDateString('en-US') +
+                                                    ' ' +
+                                                    new Date(item.createdAt)
+                                                        .toLocaleTimeString(
+                                                            'en-US'
+                                                        )
+                                                        .substring(0, 4) +
+                                                    new Date(item.createdAt)
+                                                        .toLocaleTimeString(
+                                                            'en-US'
+                                                        )
+                                                        .substring(7, 11)}
+                                            </Text>
                                         </View>
-                                    )
-                                }
-                            }}
-                            keyExtractor={(item, index) => index.toString()}
-                        />
-                    </View>
+                                    </View>
+                                );
+                            }
+                        }}
+                        keyExtractor={(item, index) => index.toString()}
+                    />
+                </View>
                 <View style={chatroomStyles.bottomContainer}>
                     <View>
                         <TextInput
                             style={chatroomStyles.input}
                             multiline={true}
                             value={this.state.newMessage}
-                            onChangeText={newMessage => {this.setState({ newMessage })}}
+                            onChangeText={newMessage => {
+                                this.setState({ newMessage });
+                            }}
                         />
                         <View style={chatroomStyles.sendButtonContainer}>
                             <TouchableOpacity onPress={this.sendMessage}>
@@ -125,5 +182,19 @@ export default class Chatroom extends Component<ChatroomProps, ChatroomState> {
                 </View>
             </KeyboardAvoidingView>
         );
+    }
+
+    private sendMessage() {
+        if (this.state.newMessage.replace(/\s/g, '').length) {
+            this.props.socket.emit(
+                'sendMessage',
+                this.props.userID,
+                this.props.groupID,
+                this.state.newMessage,
+                this.props.handleNewMessage
+            );
+            this.setState({ newMessage: '' });
+        }
+        return;
     }
 }
